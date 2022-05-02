3e3f125d6952229da04c0e349808b9a5d8243c38
Nick Santana <nick@mobilecoin.com>
2022-05-02 15:11:14 -0700

    I am Root. We are Root.
    have the code compiling need to now link against the urts library

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,22 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-extern crate libc;
-
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::ffi::CString;
+    use std::mem::MaybeUninit;
+
     #[test]
     fn create_enclave() {
-        assert_eq!(sgx_create_enclave(), 4);
+        let file_name: *const ::std::os::raw::c_char = CString::new("").unwrap().as_ptr();
+        let debug: ::std::os::raw::c_int = 0;
+        let mut launch_token: sgx_launch_token_t = [0; 1024];
+        let mut launch_token_updated: ::std::os::raw::c_int = 0;
+        let mut enclave_id: sgx_enclave_id_t = 0;
+        let mut misc_attr: sgx_misc_attribute_t = unsafe{ MaybeUninit::<sgx_misc_attribute_t>::zeroed().assume_init() };
+        assert_eq!(unsafe {sgx_create_enclave(file_name, debug, &mut launch_token as *mut sgx_launch_token_t, &mut launch_token_updated, &mut enclave_id, &mut misc_attr)}, _status_t_SGX_SUCCESS);
     }
 }
