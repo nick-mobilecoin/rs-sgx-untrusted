69d6aa83493ecc18ca9a67068d4d06bc38ac63ca
Nick Santana <nick@mobilecoin.com>
2022-05-02 15:25:02 -0700

    Feed. You. Stuff. No time.
    Have urts linking and running in a demo test

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
 // Copyright (c) 2022 The MobileCoin Foundation
 extern crate bindgen;
+use cargo_emit::{rustc_link_arg, rustc_link_search};
 
 use std::{env, path::PathBuf};
 
 fn main() {
+    let sim_postfix = "_sim";
+    rustc_link_arg!(
+        &format!("-lsgx_urts{}", sim_postfix),
+        &format!("-lsgx_launch{}", sim_postfix)
+    );
+    rustc_link_search!("/opt/intel/sgxsdk/lib64");
     // Need to evaluate if all the types should be intermixed here
     let bindings = bindgen::Builder::default()
         .header_contents("status.h", "#include <sgx_error.h>\n#include <sgx_urts.h>")
