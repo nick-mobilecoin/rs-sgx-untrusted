02d7303e8e45d4032c987be7ffb7cc85e49dd465
Nick Santana <nick@mobilecoin.com>
2022-05-04 12:48:43 -0700

    Done, to whoever merges this, good luck.
    remove some debug code

--- test_enclave/src/enclave.c
+++ test_enclave/src/enclave.c
@@ -1,17 +1,7 @@
 // Copyright (c) 2022 The MobileCoin Foundation
 /*
- * A basic functoin that just adds 2 to its input
+ * A basic function that just adds 2 to its input
  */
-
-#include <assert.h>
-#include <stdlib.h>
-#include "sgx_trts.h"
 void ecall_add_2(int input, int *sum) {
-//    *sum = input + 2;
-
-    if (sgx_is_within_enclave(sum, sizeof(int)) != 1) {
-        abort();
-    }
-    assert(*sum == 0);
     *sum = input + 2;
 }
